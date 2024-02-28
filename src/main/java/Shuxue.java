7824c18506de8a92833a857af3adb80b926a930d
lookintoforward <wuenhui173@gmail.com>
2024-02-28 16:56:57 +0800

    add aaa

--- src/main/java/Shuxue.java
+++
@@ -1,8 +0,0 @@
-public class Shuxue {
-    public static void main(String[] args) {
-        System.out.println("数学学习资料");
-        System.out.println("数学学习资料");
-        System.out.println("数学学习资料");
-        System.out.println("aaaaa");
-    }
-}
