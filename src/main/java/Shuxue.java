dd3eb0220aebb074f4277320bdabd3eedfa9c4c8
wuenhui <wuenhui173@gmail.com>
2024-02-28 15:41:39 +0800

    add 数学学习资料

---
+++ src/main/java/Shuxue.java
@@ -0,0 +1,8 @@
+public class Shuxue {
+    public static void main(String[] args) {
+        System.out.println("数学学习资料");
+        System.out.println("数学学习资料");
+        System.out.println("数学学习资料");
+
+    }
+}
