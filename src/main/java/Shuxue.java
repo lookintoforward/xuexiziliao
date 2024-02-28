f8bfc40197cf490c13e712fcd1d427f88e330f17
wuenhui173 <1136749389@qq.com>
2024-02-28 18:41:43 +0800

    添加数学学习资料

---
+++ src/main/java/Shuxue.java
@@ -0,0 +1,7 @@
+public class Shuxue {
+    public static void main(String[] args) {
+        System.out.println("数学资料");
+        System.out.println("数学资料");
+        System.out.println("数学资料");
+    }
+}
