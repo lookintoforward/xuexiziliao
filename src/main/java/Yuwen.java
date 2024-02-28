842ed263c011f189ffd547a1b81bfe0d9bbbbe47
lookintoforward <wuenhui173@gmail.com>
2024-02-28 15:22:14 +0800

    add 语文学习资料

---
+++ src/main/java/Yuwen.java
@@ -0,0 +1,7 @@
+public class Yuwen {
+    public static void main(String[] args) {
+        System.out.println("语文学习资料");
+        System.out.println("语文学习资料");
+        System.out.println("语文学习资料");
+    }
+}
