2bd4462db0b11997a073ed751aeebf6055e61b3c
wuenhui173 <1136749389@qq.com>
2024-02-28 20:51:45 +0800

    add 物理学习资料

---
+++ src/main/java/Wuli.java
@@ -0,0 +1,7 @@
+public class Wuli {
+    public static void main(String[] args) {
+        System.out.println("物理学习资料");
+        System.out.println("物理学习资料");
+        System.out.println("物理学习资料");
+    }
+}
